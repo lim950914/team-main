b1ee1ed9f36f6d9dded052cb2366348140f06df1
lim950914 <dlackswn2222@naver.com>
2021-07-15 19:54:38 +0900

    security

--- src/main/java/org/zerock/controller/member/TpMemberController.java
+++ src/main/java/org/zerock/controller/member/TpMemberController.java
@@ -1,10 +1,14 @@
 package org.zerock.controller.member;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.domain.member.TpCriteria;
 import org.zerock.domain.member.TpMemberVO;
@@ -38,6 +42,17 @@ public class TpMemberController {
 			return "redirect:/member/join?error";
 		}
 	}
+	
+	
+	
+//	@GetMapping(params = "date")
+//	  public String localDate(
+//	      @RequestParam 
+//	      @DateTimeFormat(pattern = "yyyy-MM-dd")
+//	      final LocalDate date) {
+//	    return date.toString();
+//	  }
+	
 }
 	
 	
